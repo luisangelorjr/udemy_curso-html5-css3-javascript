f241847240b25e2eb51431380f860fe4efb6deb5
Luis Angelo Rodrigues Jr <luisangelorjr@gmail.com>
2018-11-22 18:31:26 -0200

    Lesson '0210 - JavaScript com formulário' is COMPLETE
    Criação de eventListener, forms para inserir dados e trabalhar com um javascript

--- js/script.js
+++ js/script.js
@@ -1,11 +1,21 @@
 //var resposta = confirm("Deseja excluir?");
 //console.log(resposta);
-
-var a = 10;
-var b = 33;
+const a = 10;
+const b = 33;
 
 function somar(a, b){
     return a + b;
 }
 
-console.log(somar(a, b));
+//console.log(somar(a, b));
+
+document.querySelector("#calcular").addEventListener("click", function (){
+    let valorA = document.querySelector("#valorA").value;
+    let valorB = document.querySelector("#valorB").value;
+
+    if (valorA.length > 0 && valorB.length > 0){
+        alert(parseInt(valorA) + parseInt(valorB)); 
+    } else{
+        alert("Digite os valores")
+    }
+});
